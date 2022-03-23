583026055de915f59ae22790850938107b81aa52
msr <mshineray@qq.com>
2022-03-23 10:36:13 +0800

    add: vue theme

---
+++ demo/.vitepress/dist/assets/api_options-rendering.md.4d0b951f.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.e535d7cf.js";
+const __pageData = '{"title":"Component Options: Rendering","description":"","frontmatter":{},"headers":[{"level":2,"title":"template","slug":"template"},{"level":2,"title":"render","slug":"render"}],"relativePath":"api/options-rendering.md","lastUpdated":1647843469097}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 5);
+const _hoisted_6 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_6);
+}
+var optionsRendering = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, optionsRendering as default };
---
+++ demo/.vitepress/dist/assets/api_options-state.md.fc1ea8b7.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.e535d7cf.js";
+const __pageData = '{"title":"Component Options: State","description":"","frontmatter":{},"headers":[{"level":2,"title":"data","slug":"data"},{"level":2,"title":"props","slug":"props"},{"level":2,"title":"computed","slug":"computed"},{"level":2,"title":"methods","slug":"methods"},{"level":2,"title":"watch","slug":"watch"},{"level":2,"title":"emits","slug":"emits"}],"relativePath":"api/options-state.md","lastUpdated":1647843469097}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 14);
+const _hoisted_15 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_15);
+}
+var optionsState = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, optionsState as default };
