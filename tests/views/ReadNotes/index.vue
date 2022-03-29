24fcb0f0e2946f9ac1802ed14e2256c5a59823c2
msr <mshineray@qq.com>
2022-03-29 10:25:28 +0800

    upd: clean

---
+++ tests/views/ReadNotes/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div
+        class="col-sm-6 col-md-4 col-lg-3"
+        v-for="(book, index) in bookList"
+        :key="book.name"
+        style="margin-bottom: 20px"
+      >
+        <el-card shadow="hover">
+          <div class="thumbnail">
+            <a
+              :href="'#' + book.bookUrl"
+              title="Reading Notes"
+              style="width: 150px; height: 200px"
+              target="_blank"
+            >
+              <p>{{ book.bookName }}</p>
+              <!-- <img :src="'./img'+book.bookImg"  style="width: 150px;height: 200px;" alt="">-->
+            </a>
+            <div class="caption">
+              <p>{{ book.author }}</p>
+            </div>
+          </div>
+        </el-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import bookList from './bookList.json'
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+  },
+  data() {
+    return {
+      bookList,
+    }
+  },
+  created(){
+    console.log(1)
+    window.gtag_event && window.gtag_event('open-'+this.$route.path, this.$route.query)
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 20px;
+  .el-card {
+    min-height: 150px;
+  }
+}
+</style>
---
+++ tests/views/ReadNotes/weread/pingjunfen.vue
@@ -0,0 +1,381 @@
+<template>
+  <div class="container">
+    <h1 class="title">平均分</h1>
+    <h5 class="author">康辉</h5>
+    <img src="./img/pingjunfen/t6_28070306.jpg" alt="written" class="written" />
+    <div class="wr_macOS wr_Desktop wr_page_reader wr_whiteTheme">
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">广院断章</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">一切的未知，一切的不可能，都尽可以去试一下。</div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">康辉：21℃，不多不少，刚刚好</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              说是有四种人可以做一辈子的朋友：一是一起扛过枪的，二是一起下过乡的，三是一起负过伤的，四是一起同过窗的。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">江城子</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              该你做的功课提前做也是做，拖到最后做也是做；该你做的事，随手做也是做，集中到最后花费大量时间、精力、气急败坏地做也是做。随时看一眼功课就不用到期末考试时挑灯夜战了，随手把东西整理好就不用为找个小东西翻箱倒柜了，把这些事情都安排合理的话，你就很主动，很少惊慌失措，其实这才是最省力气的办法
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">我与《新闻联播》</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">说风光，南秀北雄；看年俗，东西不同。</div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">难忘的2008</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              什么是健康？世界卫生组织下的定义是“躯体健康、心理健康、社会适应良好和道德健康”
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              要是没有了残疾，健全会否因其司空见惯而变得腻烦和乏味呢？我常梦想着在人间彻底消灭残疾，但可以相信，那时将由患病者代替残疾人去承担同样的苦难。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">十年一觉世博梦</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              当我们记录今天的时候，如果还能着眼于明天，那么今天的记录也就绝不只是记录了。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">那些年，我们一起追过的神舟</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              航天人从不说“我如何”，他们会让你知道团队有多重要。做这一行的，几乎个个是精英，大概也都听过“不想当元帅的士兵不是好士兵”的名言，但他们更懂得“元帅做好元帅的事、士兵做好士兵的事”的重要性。也许明天的岗位不同了，但今天的岗位要求怎样做，我就应该怎样做。一个团队，只有各司其职，才是有战斗力的。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">此地空余老山墓——当年北京老山汉墓考古发掘直播的故事</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              我从哪里来？要到哪里去？这是人类的一个终极问题。自有人类的记忆开始，对此的追问就从未停止，尽管至今没有一个令全人类信服的答案，但不妨碍一代代地问下去。而寻找答案的重要途径之一，便是向历史深处探望，一重重地累积人类对这个问题的解题过程。在这个过程中，每一个民族都会对先辈产生一种历史敬畏，因为每一个民族都曾以伟大的创造在丰富着人类的历史，每一个民族都在历史中建立起一步步接近于回答那个人类终极问题的自信。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              当“我的工作”需要“别人的工作”来配合时，当“我的工作”与“别人的工作”有冲突时，我首先应该尊重“别人的工作”，尊重别人的规律，尊重别人的原则，而不是为了“我的工作”而无视对方。哪怕“我的工作”因此而并未达到最好的效果，我至少可以问心无愧。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">忆罗京</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              你已经是一个职业的电视播音员了，不可能再像在学校里那样，有老师每天手把手地教你、带你、督促你，你必须具备一副观察的眼光、一种学习的能力，带着你在实践中观察到的所有问题去自主学习。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              如今的时代，变化的周期只会越来越短，变化的方式只会越来越多，我们要想让自己不成为“被拍死在沙滩上的前浪”，就必须保持前瞻的眼光与先行一步的行动。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">在《新闻联播》里寻找“中国气派”</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              康辉喜欢读金庸，最喜欢令狐冲，他向往令狐冲的洒脱。但那种从心所欲，他认为是需要真正懂得如何遵从内心的人才能做到的，而不是行为上的肆意。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              “主雅客来勤”，主人很好地表现出沟通的意愿，构建了沟通的良好氛围，大家自然愿意有更多的响应。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              小事情做不好，怎么取信于人？所以年轻人焦虑是可以理解的，但别着急，踏踏实实的，你认真做的一切都会被看得到，现在积累的所有东西，到最后都是长在你自己身上的本事，没有人能够拿走。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">我不是“播神”</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              我不敢苟同，且不说任何失误都不可能不被发现，既然已经错了，不管当时有多少人注意到了，该承认并且改正就要承认和改正，这不丢人。比起把头埋在沙堆里就以为别人看不见的鸵鸟政策，坦承并及时改正不是更好的处理方式吗？
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              无限趋近完美的工作，只能靠每一次的认真仔细、小心翼翼一点点积累。我们是人，难免出错，但我们又不可以以此为借口而降低工作的标准。完美或许不存在，但追求完美的人应该存在。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">“念稿子”的人想“说”的几句话</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              有句俗话叫“看人挑担轻飘飘，自己挑担压折腰”，其实没有一件事是简单的，千万别随意地看任何一项工作。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              制度建立最大的好处就是让每个人都知道“什么是你必须做的”，而不仅仅依靠个人的自觉、热情。实践一再证明，人天生有局限，时间长了，自觉与热情都不一定靠得住。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">成长——2011年在厦门大学的演讲</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              很多时候只要迈出第一步，就会发现，事情远没有你想象的那么困难，反而是迈出第一步真的需要一点勇气。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">成长是一个不断认识自己的过程，通过认识自己去认识这个世界。</div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              最初可能会今天尝试这样，明天换个样子，但不可以总是被别人的标准影响着，慢慢地，你必须让自己的内心变得越来越恒定，你必须建立起自己的标准，我知道我应该是什么样的，我可以是什么样的。当有了这个标准的时候，就是成长了。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              青春有的是时间，为什么不可以去“挥霍”一下？如果不去做，又怎么能知道到底可不可以？没错，尽可以去试，这个过程正是一个认识自己的过程。但每一次的选择都不能是完全盲目的，每一次的选择都应该是基于那个阶段对自己的认识，而不是被一些概念或者他人对你的定义束缚住。只有不断认识自己，才有成长道路上的选择与取舍。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">成长是一个逐渐学会、懂得承担责任的过程。</div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              成长是一个慢慢懂得什么是“尊重”的过程。学会真正尊重别人，是成长的一个非常重要的标志。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">成长是一个逐渐学会质疑、不再盲从的过程。</div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              成长是一个自然的过程，但又是一个需要不断学习的过程。我所希望的是，大家不断成长，变得越来越成熟，但不要变得越来越世故。在我看来，成熟和世故是有很大区别的，最大的区别就在于，成熟是无论经历何等风雨，依然会用一种纯净的眼光看待这个世界，只不过，我会比年轻时看待世界的角度更多，看得更深更广。而世故，则完全是站在自身利害的角度，始终以一种趋利避害的态度来看待世界，看待一切的人和事，慢慢地你的眼睛就会变得越来越浑浊，看到的世界也会越来越浑浊。当人们看这个世界的目光变得很浑浊的时候，这个世界还会变得美丽、清澈吗？
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">
+          念念不忘，必有回响——在中国传媒大学2015年开学典礼上的演讲
+        </div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              在今天，人人都想飞奔，人人都唯恐落后，但是在我们飞奔之前，在我们站在起跑线上的时候，我们要想一想，是不是给自己的飞奔做好了一个完备的基础的准备？而基础的扎实往往要下很多的笨功夫和慢功夫，否则就会行百里者半九十。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">我的“成人礼”——兼怀父亲</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              我第一次体会到了什么是世事绝非尽如人愿，做好自己的本分，并不一定就能顺利收获期望的结果。很多因素，甚至是你根本无法意识到的因素，却有可能影响你的人生。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">萌宠人生</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              每一种选择都不分好坏或对错，只是要承担这个选择带来的所有结果，相信不同的人生会有各自的圆满。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">欲寄彩笺兼尺素</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              人总是缺什么想什么。当你开始怀念什么的时候，也就是它早已远去的时候。比如现在，我们习惯了飞奔，却又渴望漫步。彻底地慢下来难之又难，但让自己慢下来的生活方式，总还是有的。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_divider leftIndent"></div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              慢，才有仪式感，生活之美很大程度在于仪式感。因为仪式永远需要从容，需要一板一眼，一举手，一投足，都要多一些用心，也就多一分雅致，生活方上升为艺术。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sectionListItem">
+        <div class="sectionListItem_title">绿茶心处是江南</div>
+        <div class="sectionListItem_content noteItem_content clickable">
+          <span class="icon"></span>
+          <div>
+            <div class="text">
+              比较着，还是绿茶接近茶的根本。花茶得了个花字，总是花香占了上风；红茶需配奶和糖，好比嫁了的女儿，总归是人家的了；乌龙、普洱，浓浓的汤汁，怎么着也是多了几分烟火气。绿茶就恰到好处，炒过的，去了生涩，可也不过头，新鲜的青青的春意就锁在里面，什么时候用水一激，就一下子散了出来，能让你好像蓦地看到满坡满谷的茶树林，这又是对江南的回忆了。
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="footer">
+      <img src="./img/pingjunfen/qr.png" alt="qr" />
+      <p>扫码阅读本书</p>
+    </footer>
+  </div>
+</template>
+<script>
+export default {
+  name: 'pingjunfen',
+  created(){
+    console.log(1)
+    window.gtag_event && window.gtag_event('open-'+this.$route.path, this.$route.query)
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import '~@/assets/css/weread.css';
+@import '~@/assets/scss/readnotes';
+</style>
