fe320bbdaf58cb2c3fe5e09c3e2bfc471d7f6944
msr <mshineray@qq.com>
2024-09-24 20:59:05 +0800

    deploy

--- docs/assets/chunks/log-created.DGlCQ48K.js
+++
@@ -1,1 +0,0 @@
-const e={created(){}};export{e as l};
--- src/mixin/log-created.js
+++ src/mixin/log-created.js
@@ -3,6 +3,8 @@
  */
 export default {
   created(){
-    // window.gtag_event && window.gtag_event(location.pathname)
+    if (typeof window !== "undefined") {
+      window.gtag_event && window.gtag_event(location.pathname)
+    }
   }
 }
