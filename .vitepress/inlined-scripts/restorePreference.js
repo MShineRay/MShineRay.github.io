4e353285a53fa5f99342792f723fcd3fd0fde6f6
msr <mshineray@qq.com>
2024-09-24 20:56:33 +0800

    deploy

--- .vitepress/inlined-scripts/restorePreference.js
+++ .vitepress/inlined-scripts/restorePreference.js
@@ -1,15 +1,15 @@
-;(() => {
-  if (typeof window !== "undefined") {
-    const restore = (key, cls, def = false) => {
-      const saved = localStorage.getItem(key)
-      if (saved ? saved !== 'false' : def) {
-        document.documentElement.classList.add(cls)
-      }
-    }
-    restore('vue-docs-prefer-composition', 'prefer-composition')
-    restore('vue-docs-prefer-sfc', 'prefer-sfc', true)
-
-    window.__VUE_BANNER_ID__ = 'wip'
-    restore(`vue-docs-banner-${__VUE_BANNER_ID__}`, 'banner-dismissed')
-  }
-})()
+// ;(() => {
+//   if (typeof window !== "undefined") {
+//     const restore = (key, cls, def = false) => {
+//       const saved = localStorage.getItem(key)
+//       if (saved ? saved !== 'false' : def) {
+//         document.documentElement.classList.add(cls)
+//       }
+//     }
+//     restore('vue-docs-prefer-composition', 'prefer-composition')
+//     restore('vue-docs-prefer-sfc', 'prefer-sfc', true)
+//
+//     window.__VUE_BANNER_ID__ = 'wip'
+//     restore(`vue-docs-banner-${__VUE_BANNER_ID__}`, 'banner-dismissed')
+//   }
+// })()
